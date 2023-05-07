57505040ced056f3ed786ef0b023fd56d2270b53
Douaibi Mostafa <madaradouaibi@gmail.com>
2023-05-07 03:03:17 +0100

    acceuil recipes with stars

--- recipe/home/views.py
+++ recipe/home/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from recette.models import Recette
+from notes.models import Note
+from django.db.models import Avg
+
 
 def home(request):
-    return render(request, 'home_page.html')
+    recettes = Recette.objects.all()
+    for recette in recettes:
+        note_moyenne = Note.objects.filter(recette=recette).aggregate(Avg('valeur'))['valeur__avg']
+        recette.note_moyenne = note_moyenne
+    context = {
+        'recettes': recettes
+    }
+    return render(request, 'home_page.html', context)
